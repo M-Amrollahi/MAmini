1a6c9895675cce05a28a2259600827454f6d41bf
Mahdi Amrollahi <mhd.amrollahi@gmail.com>
2022-10-14 08:26:27 +0330

    Pages added

---
+++ Main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Main",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+st.markdown("""According to recent events in Iran due to the murdered 22-year-old girl called [__Mahsa Amini__](https://en.wikipedia.org/wiki/Death_of_Mahsa_Amini), her name has been a trend on social media platforms like Twitter(__#MahsaAmini #مهسا_امینی__). She died on September 16, 2022, and since then, many protests have been held in Iran.
+We are supposed to collect the data on Twitter related to her and analyze the data.""")
